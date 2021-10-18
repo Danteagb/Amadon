1232f2d96deda9c999571153e9d76a1efc95bda0
Dante <danteandresgb@gmail.com>
2021-10-18 16:39:46 -0300

    Agregue todo lo que tiene que ver con Amadon

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
 from .models import Order, Product
 
 def index(request):
-    context = {
-        "all_products": Product.objects.all()
-    }
-    return render(request, "store/index.html", context)
+    if request.method=="GET":
+        context = {
+            "all_products": Product.objects.all()
+        }
+        return render(request, "store/index.html", context)
+    elif request.method== "POST":
+        quantity_from_form = int(request.POST["quantity"])
+        product_id= request.POST["product_id"]
+        price_from_form = Product.objects.get(id=product_id).price
+        total_charge = quantity_from_form * price_from_form
+        print("Charging credit card...")
+        Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+        return redirect("/checkout")
 
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    context={
+    "last_order": Order.objects.last()
+}
+    return render(request, "store/checkout.html",context)
